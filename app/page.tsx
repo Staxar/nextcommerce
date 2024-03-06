ffc883e90e0417283e60197c8da84590afb7ce35
Marcin <brzeszczot90@gmail.com>
2024-03-06 12:59:09 +0100

    add carousel

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,19 @@
+import { CarouselSize } from "@/components/CarouselSize";
+import Footer from "@/components/Footer";
 import Header from "@/components/Header";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <Header />
+      <div className="flex flex-col place-items-center text-3xl text-center">
+        <h3 className="scroll-m-20 text-2xl font-extrabold tracking-tight lg:text-5xl">
+          E-commerce with NextJS
+        </h3>  
+      <CarouselSize />
+      </div>
+      <Footer />
+
 
 
     </main>
