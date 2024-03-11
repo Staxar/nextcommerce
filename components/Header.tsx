b18fce2ff3053239b37faed8336b57ac101a5d45
Marcin <brzeszczot90@gmail.com>
2024-03-11 17:09:48 +0100

    header

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,18 +1,28 @@
 import React from 'react'
 import { Avatar, AvatarFallback, AvatarImage } from './ui/avatar'
+import { ShoppingBasket } from 'lucide-react'
+import Link from 'next/link'
+import { HeaderMenu } from './HeaderMenu'
 
 function Header() {
-  return (
-    <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-            
-            <Avatar>
-              <AvatarImage src="https://github.com/Staxar.png" />
-              <AvatarFallback><small className="text-sm font-medium leading-none">MD</small></AvatarFallback>
-            </Avatar>
-        </p>
-  </div>
-  )
+    return (
+        <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
+            <div className="fixed left-0 top-0 flex w-full items-center justify-between p-8 border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-full  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
+                <HeaderMenu />
+                <div className="flex items-center gap-4">
+                    <Avatar>
+                        <AvatarImage src="https://github.com/Staxar.png" />
+                        <AvatarFallback>
+                            <small className="text-sm font-medium leading-none">
+                                MD
+                            </small>
+                        </AvatarFallback>
+                    </Avatar>
+                    <ShoppingBasket size={24} />
+                </div>
+            </div>
+        </div>
+    )
 }
 
 export default Header
