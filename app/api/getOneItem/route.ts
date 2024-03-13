63ab5a722cd1e90b55af53caaaa0d203f2cc8e5f
Marcin <brzeszczot90@gmail.com>
2024-03-13 11:29:24 +0100

    fix oneItem

--- app/api/getOneItem/route.ts
+++ app/api/getOneItem/route.ts
@@ -1,21 +1,18 @@
-import { connectDB } from '@/services/connectDB'
-import { Connection } from '../getData/route'
+import { MongoClient } from 'mongodb'
 
 export async function GET(request: Request) {
     const { searchParams } = new URL(request.url)
+    const MONGODB_URI = process.env.MONGODB_URI
+    if (MONGODB_URI) {
+        const client = new MongoClient(MONGODB_URI)
+        await client.connect()
+        const database = client.db('nextcommerce')
+        const collection = database.collection('nextcommerce')
 
-    const connection: Connection | null = await connectDB()
+        const res = await collection.findOne(searchParams)
 
-    const res = await connection?.collection.findOne(searchParams)
-
-    return Response.json(res)
-    // try {
-    //     const res = await connection.collection.findOne(searchParams)
-
-    //     return Response.json(res)
-    // } catch (error) {
-    //     return Response.error()
-    // } finally {
-    //     await connection.client.close()
-    // }
+        return Response.json({ res })
+    } else {
+        return Response.error()
+    }
 }
