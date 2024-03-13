e9f1520675969c1aae124c46232514b034bdbc58
Marcin <brzeszczot90@gmail.com>
2024-03-13 20:15:48 +0100

    limitData

--- app/api/getLimitData/route.ts
+++ app/api/getLimitData/route.ts
@@ -1,16 +1,18 @@
 import { MongoClient } from 'mongodb'
-import { NextResponse } from 'next/server'
+import { NextRequest, NextResponse } from 'next/server'
 
 export const dynamic = 'force-dynamic'
-export async function GET() {
+export async function GET(request: NextRequest) {
     const MONGODB_URI = process.env.MONGODB_URI
-    if (MONGODB_URI) {
+    const { searchParams } = new URL(request.url)
+    const limitData = Number(searchParams) || 5
+    if (MONGODB_URI && limitData) {
         const client = new MongoClient(MONGODB_URI)
         await client.connect()
         const database = client.db('nextcommerce')
         const collection = database.collection('nextcommerce')
 
-        const data = await collection.find().toArray()
+        const data = await collection.find().limit(limitData).toArray()
 
         return NextResponse.json(data)
     } else {
