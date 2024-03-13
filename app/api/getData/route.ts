d60bebf565d929a19a274d040a2fc9e8d0ba34d9
Marcin <brzeszczot90@gmail.com>
2024-03-13 20:09:54 +0100

    dynamic

--- app/api/getData/route.ts
+++ app/api/getData/route.ts
@@ -1,15 +1,17 @@
 import { MongoClient } from 'mongodb'
 import { NextResponse } from 'next/server'
 
-const MONGODB_URI = process.env.MONGODB_URI || ''
-
+export const dynamic = 'force-dynamic'
 export async function GET() {
-    const client = new MongoClient(MONGODB_URI)
-    await client.connect()
-    const database = client.db('nextcommerce')
-    const collection = database.collection('nextcommerce')
+    const MONGODB_URI = process.env.MONGODB_URI
+    if (MONGODB_URI) {
+        const client = new MongoClient(MONGODB_URI)
+        await client.connect()
+        const database = client.db('nextcommerce')
+        const collection = database.collection('nextcommerce')
 
-    const data = await collection.find().toArray()
+        const data = await collection.find().toArray()
 
-    return NextResponse.json(data)
+        return NextResponse.json(data)
+    }
 }
