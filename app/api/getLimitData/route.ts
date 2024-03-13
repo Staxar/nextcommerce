3c70c4898856c9c6bd739e04b5f071ac6f4ca9c6
Marcin <brzeszczot90@gmail.com>
2024-03-13 20:33:46 +0100

    add await

--- app/api/getLimitData/route.ts
+++ app/api/getLimitData/route.ts
@@ -25,6 +25,6 @@ export async function GET(request: NextRequest) {
         console.error('Error:', error)
         return NextResponse.error()
     } finally {
-        client.close()
+        await client.close()
     }
 }
