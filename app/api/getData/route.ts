5927e65035a986d5ecc0eb664ab1b085cf45efed
Marcin <brzeszczot90@gmail.com>
2024-03-13 20:01:24 +0100

    change

--- app/api/getData/route.ts
+++ app/api/getData/route.ts
@@ -1,19 +1,15 @@
 import { MongoClient } from 'mongodb'
 import { NextResponse } from 'next/server'
 
-const MONGODB_URI = process.env.MONGODB_URI || null
+const MONGODB_URI = process.env.MONGODB_URI || ''
 
 export async function GET() {
-    if (MONGODB_URI) {
-        const client = new MongoClient(MONGODB_URI)
-        await client.connect()
-        const database = client.db('nextcommerce')
-        const collection = database.collection('nextcommerce')
+    const client = new MongoClient(MONGODB_URI)
+    await client.connect()
+    const database = client.db('nextcommerce')
+    const collection = database.collection('nextcommerce')
 
-        const data = await collection.find().toArray()
+    const data = await collection.find().toArray()
 
-        return NextResponse.json(data)
-    } else {
-        return NextResponse.error()
-    }
+    return NextResponse.json(data)
 }
